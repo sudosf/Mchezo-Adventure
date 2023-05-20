e381f02483e7ef0048074c62eac18e29caba9dbd
sudosf <sudosprogrammer@gmail.com>
2023-05-20 19:42:56 +0200

    fixed font data and display

--- dev/scripts/menu.gd
+++ dev/scripts/menu.gd
@@ -1,20 +1,8 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MarginContainer/VBoxContainer/StartButton.grab_focus()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://dev/scenes/choose_country.tscn")
@@ -25,7 +13,6 @@ func _on_OptionsButton_pressed():
 	get_tree().current_scene.add_child(option)
 	pass
 	
-
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
